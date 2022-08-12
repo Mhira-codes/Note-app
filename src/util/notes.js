35753597a9b7c4c34c73a6bdea5a391c2eb1d66a
mhira-codes <alhassansamiratu991@gmail.com>
2022-08-12 17:26:37 +0000

    added objects

---
+++ src/util/notes.js
@@ -0,0 +1,46 @@
+const notes = [
+    {
+        id: 1,
+        message: "This is my first note",
+        date:"2022-08-11"
+    },
+    {
+        id: 1,
+        message: "This is my second note",
+        date:"2022-08-11"
+    },
+    {
+        id: 1,
+        message: "This is my third note",
+        date:"2022-08-11"
+    },
+    {
+        id: 1,
+        message: "This is my fourth note",
+        date:"2022-08-11"
+    },
+    {
+        id: 1,
+        message: "This is my first note",
+        date:"2011-04-21"
+    },
+    {
+        id: 1,
+        message: "This is my six note",
+        date:"2013-05-09"
+    },
+    {
+        id: 1,
+        message: "This is my seventh note",
+        date:"2012-05-01"
+    },
+    {
+        id: 1,
+        message: "This is my eighth note",
+        date:"2022-08-11"
+    },
+   
+  
+]
+
+export default notes
